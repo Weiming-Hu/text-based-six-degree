d3e2a4bebac80a7f453cd945002ff45b9c21fc5b
Weiming <cosmos.weiming@outlook.com>
2017-08-14 00:32:35 -0400

    add the first solution

---
+++ develop_ideas.py
@@ -0,0 +1,39 @@
+# this script will implement and test some ideas for how to carry out the research
+
+# use google search interface package
+#
+# this is referenced from
+# https://breakingcode.wordpress.com/2010/06/29/google-search-python/
+#
+# package document can be found at
+# http://pythonhosted.org/google/
+#
+# package repository can be found at
+# https://pypi.python.org/pypi/google
+#
+from google import search
+
+# parse html
+from bs4 import BeautifulSoup
+from urllib import urlopen
+
+# find sentences from texts
+import re
+
+# customized function
+import find_neighbors_in_sentence
+
+for url in search('"Breaking Code" WordPress blog', stop=20):
+    print(url)
+
+url = "http://news.bbc.co.uk/2/hi/health/2284783.stm"
+html = urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+target = 'was'
+sentences = re.findall(r"(.*? " + target + r" .*?\.)", soup.get_text())
+
+friends = []
+for sentence in sentences:
+    neis = find_neighbors_in_sentence(target, sentence)
+    friends.append(neis)
---
+++ find_neighbors_in_sentence.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# functions
+import re
+
+def find_neighbors_in_sentence(target, s, strict=False):
+    # find the neighbors in the sentence and return them in a list
+    #
+    # if strick is True, the function try to find the exact match
+    # for the target word without changing cases
+    #
+    neis = []
+
+    # some cleaning
+    s, number = re.subn(re.compile('[ ][ ]+'), ' ', s)
+    s, number = re.subn(re.compile('[ ]$'), '', s)
+    s, number = re.subn(re.compile('^[ ]'), '', s)
+
+    # match the target word(s) and its previous/following neighbors
+    pairs = re.findall(r"([a-zA-Z0-9]+) " + target + r" ([a-zA-Z0-9]+)[ \.]", s)
+
+    for pair in pairs:
+        for word in pair:
+            neis.append(word)
+
+    return neis
